a457b3a221d91c80aa5a6e034ef0434f2adbd349
Ronald Jabouin <37760008+ronjj@users.noreply.github.com>
2022-12-29 11:30:10 -0500

    added firebase to project using swift package manager

--- Verlo/VerloApp.swift
+++ Verlo/VerloApp.swift
@@ -6,16 +6,28 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
-struct VerloApp: App {
-    
-    @StateObject private var vm = PostViewModel()
-    
-    var body: some Scene {
-        WindowGroup {
-            AppTabBarView()
-                .environmentObject(vm)
-        }
-    }
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+@StateObject private var vm = PostViewModel()
+
+
+  var body: some Scene {
+    WindowGroup {
+        AppTabBarView()
+            .environmentObject(vm)    }
+  }
 }
